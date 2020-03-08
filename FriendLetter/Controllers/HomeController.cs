65fbd39275075294b2d668524c8450e692facc99
Keturah Howard <keturah.dev@gmail.com>
2020-03-08 15:35:17 -0700

    create a new letter variable, and implement it into view and control

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace FriendLetter.Controllers
     public ActionResult Letter() {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
       return View(myLetterVariable);
     }
 
